bf9e56416b50286e3e31c3505f286366798b46a0
ricardosilva13 <ricardo.b.silva13@gmail.com>
2015-09-22 15:27:22 -0300

    changes in controller person and serializer person

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -4,17 +4,19 @@ class Api::V1::PeopleController < ApplicationController
   # GET /api/v1/people
   # GET /api/v1/people.json
   def index
-    @api_v1_people = Api::V1::Person.all
+    @api_v1_people = Person.all
+    render json: @api_v1_people
   end
 
   # GET /api/v1/people/1
   # GET /api/v1/people/1.json
   def show
+    render json: @api_v1_person
   end
 
   # GET /api/v1/people/new
   def new
-    @api_v1_person = Api::V1::Person.new
+    @api_v1_person = Person.new
   end
 
   # GET /api/v1/people/1/edit
@@ -24,51 +26,46 @@ class Api::V1::PeopleController < ApplicationController
   # POST /api/v1/people
   # POST /api/v1/people.json
   def create
-    @api_v1_person = Api::V1::Person.new(api_v1_person_params)
+    @api_v1_person = Person.new(api_v1_person_params)
 
-    respond_to do |format|
+    
       if @api_v1_person.save
-        format.html { redirect_to @api_v1_person, notice: 'Person was successfully created.' }
-        format.json { render :show, status: :created, location: @api_v1_person }
+        render json: @api_v1_person
       else
-        format.html { render :new }
-        format.json { render json: @api_v1_person.errors, status: :unprocessable_entity }
+        render json: @api_v1_person.errors
       end
-    end
+    
   end
 
   # PATCH/PUT /api/v1/people/1
   # PATCH/PUT /api/v1/people/1.json
   def update
-    respond_to do |format|
+    
       if @api_v1_person.update(api_v1_person_params)
-        format.html { redirect_to @api_v1_person, notice: 'Person was successfully updated.' }
-        format.json { render :show, status: :ok, location: @api_v1_person }
+        render json: @api_v1_person
       else
-        format.html { render :edit }
-        format.json { render json: @api_v1_person.errors, status: :unprocessable_entity }
+        render json: @api_v1_person.errors
       end
-    end
+    
   end
 
   # DELETE /api/v1/people/1
   # DELETE /api/v1/people/1.json
   def destroy
-    @api_v1_person.destroy
-    respond_to do |format|
-      format.html { redirect_to api_v1_people_url, notice: 'Person was successfully destroyed.' }
-      format.json { head :no_content }
+    if @api_v1_person.destroy
+      render json: 'deletado'
     end
+    
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_api_v1_person
-      @api_v1_person = Api::V1::Person.find(params[:id])
+      @api_v1_person = Person.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def api_v1_person_params
-      params[:api_v1_person]
+      params.require(:person).permit(:name, :cpf, :ip_person, :email, :doer, :buyer)
     end
 end
--- app/serializers/api/v1/person_serializer.rb
+++ app/serializers/api/v1/person_serializer.rb
@@ -1,3 +1,3 @@
 class Api::V1::PersonSerializer < ActiveModel::Serializer
-  attributes :id ,:name , :cpf , :ip_person
+  attributes :id ,:name , :cpf , :ip_person, :email, :doer , :buyer
 end
